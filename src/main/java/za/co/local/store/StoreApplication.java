4bd49fd2efcb327ce015c06aca6edcc06ce8db13
oarona <oarona.komane@gmail.com>
2018-09-25 19:29:34 +0200

    Initial

---
+++ src/main/java/za/co/local/store/StoreApplication.java
@@ -0,0 +1,12 @@
+package za.co.local.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+}
